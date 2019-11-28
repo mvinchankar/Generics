b5c0bff1e77f69b788e8764a1fd4298b648c9af2
mvinchankar <mvinchankar18@gmail.com>
2019-11-28 17:17:48 +0530

    REFACTORED Refactor to create

--- src/main/java/com/bridgelabz/Maximum.java
+++ src/main/java/com/bridgelabz/Maximum.java
@@ -1,15 +1,24 @@
 package com.bridgelabz;
 
-public class Maximum<T extends Comparable> {
-    T maximum;
+public class Maximum<T,U,V extends Comparable> {
+    T maximumT;
+    U maximumU;
+    V maximumV;
 
-    public T getMaximum(T a, T b, T c) {
-        maximum = a;
+    public Maximum(T maximumT, U maximumU, V maximumV) {
+        this.maximumT = maximumT;
+        this.maximumU = maximumU;
+        this.maximumV = maximumV;
+    }
+
+
+    public static  <T extends Comparable> T getMaximum(T a, T b, T c) {
+        T maximum = a;
         if (b.compareTo(maximum) > 0) {
             maximum = b;
         }
         if (c.compareTo(maximum) > 0) {
-            maximum = c;
+            maximum =  c;
         }
         return maximum;
     }
--- src/test/java/com/bridgelabz/MaximumTest.java
+++ src/test/java/com/bridgelabz/MaximumTest.java
@@ -7,20 +7,17 @@ public class MaximumTest {
 
     @Test
     public void when_Entered_Should_Return_Maximum() {
-        Maximum maximum=new Maximum();
-        Integer result= (Integer) maximum.getMaximum(2,1,0);
+        Integer result= (Integer) Maximum.getMaximum(2,1,0);
         Assert.assertEquals(Integer.valueOf(2), result);
     }
     @Test
     public void when_Entered_Float_Should_Return_Maximum() {
-        Maximum maximum=new Maximum();
-        Float result= (Float) maximum.getMaximum(2.52f,1.41f,0.81f);
+        Float result= (Float) Maximum.getMaximum(2.52f,1.41f,0.81f);
         Assert.assertEquals(Float.valueOf(2.52f), result);
     }
     @Test
     public void when_Entered_String_Should_Return_Maximum() {
-        Maximum maximum=new Maximum();
-        String result= (String) maximum.getMaximum("Mangesh","Mahendra","Akshay");
+        String result= (String) Maximum.getMaximum("Mangesh","Mahendra","Akshay");
         Assert.assertEquals("Mangesh", result);
     }
 
