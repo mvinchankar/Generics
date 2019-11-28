bda25e38258f2fa2af8474db7f9ab6b95ad01229
mvinchankar <mvinchankar18@gmail.com>
2019-11-28 18:04:33 +0530

    ADDED printMax condition to print internally

--- src/main/java/com/bridgelabz/Maximum.java
+++ src/main/java/com/bridgelabz/Maximum.java
@@ -1,18 +1,18 @@
 package com.bridgelabz;
 
-public class Maximum<T,U,V extends Comparable> {
-    T maximumT;
-    U maximumU;
-    V maximumV;
+public class Maximum<T extends Comparable> {
+    private T maximumT;
+    private T maximumU;
+    private T maximumV;
 
-    public Maximum(T maximumT, U maximumU, V maximumV) {
+    public Maximum(T maximumT, T maximumU, T maximumV) {
         this.maximumT = maximumT;
         this.maximumU = maximumU;
         this.maximumV = maximumV;
     }
 
 
-    public static  <T extends Comparable> T getMaximum(T a, T b, T c) {
+    public static  <T extends Comparable> T testMax(T a, T b, T c) {
         T maximum = a;
         if (b.compareTo(maximum) > 0) {
             maximum = b;
@@ -20,7 +20,17 @@ public class Maximum<T,U,V extends Comparable> {
         if (c.compareTo(maximum) > 0) {
             maximum =  c;
         }
+        printMax(maximum);
         return maximum;
+    }
+    public <T> T testMax(){
+        T maxElememt= (T) testMax(this.maximumT, this.maximumU, this.maximumV);
+        printMax(maxElememt);
+        return maxElememt;
+
+    }
+    private static <T> void printMax(T maximumValue){
+        System.out.println(maximumValue);
     }
 }
 
--- src/test/java/com/bridgelabz/MaximumTest.java
+++ src/test/java/com/bridgelabz/MaximumTest.java
@@ -7,18 +7,27 @@ public class MaximumTest {
 
     @Test
     public void when_Entered_Should_Return_Maximum() {
-        Integer result= (Integer) Maximum.getMaximum(2,1,0);
+        Integer result = (Integer) Maximum.testMax(2, 1, 0);
         Assert.assertEquals(Integer.valueOf(2), result);
     }
-    @Test
-    public void when_Entered_Float_Should_Return_Maximum() {
-        Float result= (Float) Maximum.getMaximum(2.52f,1.41f,0.81f);
-        Assert.assertEquals(Float.valueOf(2.52f), result);
-    }
+
     @Test
     public void when_Entered_String_Should_Return_Maximum() {
-        String result= (String) Maximum.getMaximum("Mangesh","Mahendra","Akshay");
+        String result = (String) Maximum.testMax("Mangesh", "Mahendra", "Akshay");
         Assert.assertEquals("Mangesh", result);
+    }
+
+    @Test
+    public void when_Entered_Float_Should_Return_Maximum() {
+        Float result = (Float) Maximum.testMax(2.52f, 1.41f, 0.81f);
+        Assert.assertEquals(Float.valueOf(2.52f), result);
+    }
+
+    @Test
+    public void when_Entered_Should_Return_Maximum_With_PrintMax() {
+        Maximum max=new Maximum<Integer>(2,1,0);
+        Object maximum=max.testMax();
+        Assert.assertEquals((Integer)2,maximum);
     }
 
 }
