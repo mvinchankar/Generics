4d1f145565d3bce49ea37964d23d506076bb5fec
mvinchankar <mvinchankar18@gmail.com>
2019-11-28 16:23:35 +0530

    ADDED given float to find maximum

--- src/main/java/com/bridgelabz/Maximum.java
+++ src/main/java/com/bridgelabz/Maximum.java
@@ -13,6 +13,19 @@ public class Maximum {
         }
         return maximum;
     }
+    Float maximumFloat;
+
+    public Float getMaximum(Float a, Float b, Float c) {
+        maximumFloat=a;
+        if (b.compareTo(maximumFloat) > 0) {
+            maximumFloat = b;
+        }
+        if (c.compareTo(maximumFloat)>0) {
+            maximumFloat = c;
+        }
+        return maximumFloat;
+    }
+
 
 }
 
--- src/test/java/com/bridgelabz/MaximumTest.java
+++ src/test/java/com/bridgelabz/MaximumTest.java
@@ -11,4 +11,10 @@ public class MaximumTest {
         Integer result= maximum.getMaximum(2,1,0);
         Assert.assertEquals(Integer.valueOf(2), result);
     }
+    @Test
+    public void when_Entered_Float_Should_Return_Maximum() {
+        Maximum maximum=new Maximum();
+        Float result= maximum.getMaximum(2.52f,1.41f,0.81f);
+        Assert.assertEquals(Float.valueOf(2.52f), result);
+    }
 }
